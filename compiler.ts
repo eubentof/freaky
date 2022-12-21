9b2f2f7f6f6888c1a8fe87946b7ac55968a4f2fb
Filipe Bento <dev.bento@gmail.com>
2022-12-20 22:12:16 -0300

    partial syntax tree builder

--- compiler.ts
+++ compiler.ts
@@ -2,10 +2,31 @@ import { pipeline } from 'https://deno.land/x/compose@1.0.0/index.js'
 
 const fileContent = await Deno.readTextFile("./src/Container.fk");
 
+interface TokensTree {
+  imports?: any
+  exports?: any
+  components?: any[]
+}
+interface SegmentNode {
+  segment: string
+  childen?: SegmentNode[]
+}
+
 interface StreamNode {
   stream: string
-  exports?: any[]
-  segments?: any
+  segments?: SegmentNode[]
+  tokensTree?: TokensTree
+}
+
+enum ComponentType {
+  StateLess = 'SL',
+  StateFull = 'SF',
+}
+interface ComponentToken {
+  name: string,
+  interface: SegmentNode
+  data?: SegmentNode[],
+  type: ComponentType
 }
 
 enum NodeType {
@@ -21,35 +42,91 @@ function removeComments(node: StreamNode): StreamNode {
   return { stream: streamWithoutComments.trim() }
 }
 
-function segmentContent({ stream }: StreamNode): StreamNode {
+function buildSegmentNode(segments: string[], depth = 0): SegmentNode[] {
+
+  let segmentNode: any = { segment: '', childen: [] }
+
+  const nthDepthSegments: any = []
+  segments.forEach(segment => {
+    if (!segment) return
+
+    if (segment.startsWith('  '.repeat(depth + 1))) return segmentNode.childen.push(segment)
+
+    if (segment.startsWith('  '.repeat(depth))) {
+      const segmentName = segment.trim()
+      if (segmentNode.segment) {
+        nthDepthSegments.push(segmentNode)
+        segmentNode = { segment: segmentName, childen: [] }
+        return
+      }
+
+      segmentNode.segment = segmentName
+    }
+  })
+
+  nthDepthSegments.push(segmentNode)
+
+  nthDepthSegments.forEach((segmentedNode: any) => {
+    if (segmentedNode.childen.length) segmentedNode.childen = buildSegmentNode(segmentedNode.childen, depth + 1)
+    else delete segmentedNode.childen
+  })
+
+  return nthDepthSegments
+}
+
+function segmentContentByDepth({ stream }: StreamNode): StreamNode {
 
   const tabStream = stream.split('\n')
 
-  const segments: string[][] = []
-  let currSegment: string[] = []
-  tabStream.forEach(word => {
-    if (!word) return
-
-    if (word.startsWith('  ')) return currSegment.push(word)
-
-    if (currSegment.length) segments.push(currSegment)
-    currSegment = []
-    currSegment.push(word)
-  })
-  
-  segments.push(currSegment)
+  const segments = buildSegmentNode(tabStream)
 
   return { stream, segments }
 }
 
+function tokenizeComponents(name: string, segments: SegmentNode[]) {
+
+  const component: ComponentToken = {
+    type: name.startsWith("%") ? ComponentType.StateLess : ComponentType.StateFull,
+    name: name.replace("%", "").replace(":", ""),
+    interface: segments.pop() ?? { segment: 'ERROR' },
+  }
+
+  if (component.type == ComponentType.StateFull) component.data = segments
+
+  return component
+}
+
+function generateTokens({ segments, stream }: StreamNode): StreamNode {
+  const tokensTree: TokensTree = { components: [] }
+
+  if (!segments) return { stream }
+
+  segments.forEach(({ segment, childen = [] }) => {
+    switch (true) {
+      case segment.startsWith('import'):
+        tokensTree.imports = childen
+        break
+      case segment.startsWith('export'):
+        tokensTree.exports = childen.map((node) => node.segment)
+        break
+      default:
+        tokensTree.components?.push(tokenizeComponents(segment, childen))
+    }
+  })
+
+  return { stream, tokensTree }
+}
+
 const stream: StreamModifier = pipeline(
   removeComments,
-  segmentContent,
+  segmentContentByDepth,
+  generateTokens,
 )
 
-console.log(JSON.stringify(fileContent.replaceAll('  ', ' \t ')));
+// console.log(JSON.stringify(fileContent.replaceAll('  ', ' \t ')));
 
 const sintaxTree = stream({ stream: fileContent })
 
-console.log(sintaxTree);
+console.log(JSON.stringify(sintaxTree, null, 2));
+// console.log(sintaxTree);
 
