11f143fc11e6b5046260b3dcc93aeadc4cd611e6
Filipe Bento <dev.bento@gmail.com>
2022-12-21 10:04:51 -0300

    remove dist folder + minify html

--- build.ts
+++ build.ts
@@ -188,8 +188,9 @@ function buildComponentSegment(node: SegmentNode): string {
 
   let childrenSnipet = ''
   if (node.childen)
-    childrenSnipet = node.childen.map(child => buildComponentSegment(child)).join('\n')
-  return htmlSnipet.open + '\n' + childrenSnipet + (htmlSnipet.close ?? '') + '\n'
+    childrenSnipet = node.childen.map(child => buildComponentSegment(child)).join('')
+
+  return htmlSnipet.open + childrenSnipet + (htmlSnipet.close ?? '')
 }
 
 function buildComponentInterfaces(tree: TokensTree<ComponentToken>): TokensTree<ComponentTokenBuild> {
@@ -218,20 +219,7 @@ function buildHtml(tree: TokensTree<ComponentTokenBuild>): string {
 
   const appContent = Object.values(components).map(component => component.interface).join('')
 
-  return `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-  </head>
-  <body>
-    ${appContent}
-  </body>
-  </html>
-  `
+  return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body>${appContent}</body></html>`
 }
 
 const stream: StreamModifier = pipeline(
